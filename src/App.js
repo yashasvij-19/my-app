82662109cdb031d71cbd22b3b090945a72e7a96f
Yashasvi <yashasvivij01@gmail.com>
2023-08-08 20:57:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+const App = () => {
+const [count,setCount] = React.useState(0);
+  function changeShape(){
+    var x = document.getElementById("mySelect").value;
+    var y = document.getElementById("shapes-holder");
 
-function App() {
+    if(x === "square"){
+      let divS = document.createElement("div");
+      divS.className = "square";
+      divS.textContent = count;
+      setCount(count+1);
+      y.appendChild(divS);
+
+    }else if(x === "circle"){
+let divC = document.createElement("div");
+divC.className = "circle";
+divC.textContent = count;
+setCount(count+1);
+y.appendChild(divC);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div id="shape-creator">
+        <select id="mySelect" defaultValue="square">
+  <option value="square">Square</option>
+  <option value="circle">Circle</option>
+</select>
+<button onClick={changeShape}>Add shape</button>
+      </div>
+      <div id="shapes-holder">
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
